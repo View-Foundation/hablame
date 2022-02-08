36df8438895356d3d492ddb9e57c933bd5984097
Andrés Mauricio Gallego <mauriciog98@yahoo.es>
2022-02-08 16:11:37 -0500

    error on POST request

--- src/HablameServiceProvider.php
+++ src/HablameServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace NotificationChannels\Hablame;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
 class HablameServiceProvider extends ServiceProvider
@@ -13,21 +14,12 @@ class HablameServiceProvider extends ServiceProvider
     {
         // Bootstrap code here.
 
-        /**
-         * Here's some example code we use for the pusher package.
-
-        $this->app->when(Channel::class)
-            ->needs(Pusher::class)
+        $this->app->when(HablameChannel::class)
+            ->needs(Client::class)
             ->give(function () {
-                $pusherConfig = config('broadcasting.connections.pusher');
-
-                return new Pusher(
-                    $pusherConfig['key'],
-                    $pusherConfig['secret'],
-                    $pusherConfig['app_id']
-                );
+                return new Client();
             });
-         */
+
 
     }
 
